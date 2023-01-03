b501e28abc6dc69bf4a6ca61f19b9005d05b6129
Anita Yang <anita@Anitas-MacBook-Pro-4.local>
2023-01-03 17:33:10 +1100

    Added some unit tests

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -1,10 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import LoginForm from '.';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import LoginForm from ".";
 
-test('renders sign in page', () => {
+test("renders sign in page", () => {
   render(<LoginForm />);
   const signInText = screen.getByText("Sign in");
   expect(signInText).toBeInTheDocument();
 });
 
 // Add more unit test here
+
+test("Error arises without adding required password field", () => {
+  render(<LoginForm />);
+  userEvent.click(
+    screen.getByRole("button", {
+      name: /sign in/i,
+    })
+  );
+  expect(
+    screen.getByText("Password should be 8 or more characters")
+  ).toBeInTheDocument();
+});
+
+test("Error arises without adding required email field", () => {
+  render(<LoginForm />);
+  userEvent.click(
+    screen.getByRole("button", {
+      name: /sign in/i,
+    })
+  );
+  expect(
+    screen.getByText("Please enter valid email address")
+  ).toBeInTheDocument();
+});
